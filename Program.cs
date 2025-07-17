fe4d0d93a67fc54e19e8da63326504486c80899f
Jake Boraman <jake.boraman@gmail.com>
2025-07-17 15:07:28 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using Bungie;
+using Bungie.Tags;
+using System.Text.RegularExpressions;
+
+namespace AIDialogueUpdater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // MB Initialisation
+            string h3ek = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\H3EK";
+            ManagedBlamSystem.InitializeProject(InitializationType.TagsOnly, h3ek);
+
+            while (true)
+            {
+                // Get tag path
+                Console.WriteLine("Enter tags-relative path to .ai_mission_dialogue tag, without extension: ");
+                string tagPathString = NormalisePath(Console.ReadLine());
+                TagPath tagPath = TagPath.FromPathAndExtension(tagPathString, "ai_mission_dialogue");
+
+                try
+                {
+                    using (TagFile tagFile = new TagFile(tagPath))
+                    {
+                        // Get original part of string to replace
+                        Console.WriteLine("Enter section of path to be replaced: ");
+                        string toReplace = NormalisePath(Console.ReadLine());
+
+                        // Get replacement part
+                        Console.WriteLine("Enter replacement part for new path: ");
+                        string replaceWith = NormalisePath(Console.ReadLine());
+
+                        int linesCount = ((TagFieldBlock)tagFile.SelectField("Block:lines")).Elements.Count;
+                        Console.WriteLine($"Dialogue tag has {linesCount} lines!");
+
+                        for (int i = 0; i < linesCount; i++)
+                        {
+                            string name = ((TagFieldElementStringID)tagFile.SelectField($"Block:lines[{i}]/StringID:name")).Data.ToString().Trim();
+                            int variantsCount = ((TagFieldBlock)tagFile.SelectField($"Block:lines[{i}]/Block:variants")).Elements.Count;
+
+                            for (int j = 0; j < variantsCount; j++)
+                            {
+                                TagPath soundReference = ((TagFieldReference)tagFile.SelectField($"Block:lines[{i}]/Block:variants[{j}]/Reference:sound")).Path;
+                                string updatedPath = soundReference.RelativePath.Replace(toReplace, replaceWith);
+                                TagPath newSoundReference = TagPath.FromPathAndExtension(updatedPath, "sound");
+                                ((TagFieldReference)tagFile.SelectField($"Block:lines[{i}]/Block:variants[{j}]/Reference:sound")).Path = newSoundReference;
+                            }
+
+                            Console.WriteLine("\"" + name + "\"...done!");
+                        }
+                        try
+                        {
+                            tagFile.Save();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"TAG SAVE ERROR - Failed to save tag properly. Please try closing the EK tools, then re-run this program and try again.\nException: {ex}\n");
+                            Console.WriteLine("Press enter to exit...");
+                            Console.ReadLine();
+                        }
+
+                        Console.WriteLine("\nTagfile successfully edited and saved! Press enter to exit...");
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+                catch (TagLoadException ex)
+                {
+                    Console.WriteLine($"\nTAG LOAD EXCEPTION - Perhaps you entered an invalid tag path?\nException: {ex}\n");
+                    Console.WriteLine("Please try again.");
+                }
+            }
+
+            
+        }
+
+        static string NormalisePath(string input)
+        {
+            string unified = input.Replace('/', '\\');
+            return Regex.Replace(unified, @"\\{2,}", @"\");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# AI Mission Dialogue Path Updater
+Small C# ManagedBlam tool for quickly bulk updating filepaths in a `.ai_mission_dialogue` tag.
+Replaces an existing substring of the filepath with another.
+E.g. "sound\h2_sound\sound\dialog\levels\03_earthcity\mission\l03_8070_grf" -> "sound\h2_03a\dialog\levels\03_earthcity\mission\l03_8070_grf"
+
+# Requirements
+* Requires [.NET 4.8 Runtime](https://dotnet.microsoft.com/en-us/download/dotnet-framework/net48)
+
+# Usage
+* Place `AIDialogueUpdater.exe` into a folder alongside `ManagedBlam.dll` (can be found in `H3EK\bin`)
+* Run the program
+* Enter the tags-relative path to the `.ai_mission_dialogue` tag, e.g. `halo_2\levels\singleplayer\oldmombasa\oldmombasa_dialog`
+* Enter the substring to be replaced, e.g. `sound\h2_sound\sound`
+* Enter the string to replace with, e.g. `sound\h2_03a`
+* The program will do its thing, hit enter to close when done
